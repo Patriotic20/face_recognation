5d3942682c24756bb56809dc42b6bff57c4627c9
Patriotic20 <pterest160@gmail.com>
2025-08-21 15:40:58 +0500

    Add cors

--- HikiVisionFastApi/src/main.py
+++ HikiVisionFastApi/src/main.py
@@ -1,13 +1,29 @@
 from fastapi import FastAPI
 from core.config import settings
 from consumer import lifespan
+from fastapi.middleware.cors import CORSMiddleware
 from user_logs.api import router as user_logs_router
+from user.api import router as user_router 
+from auth.api import router as auth_router
 import uvicorn
 
 app = FastAPI(lifespan=lifespan)
 
+origins = [
+    "http://localhost:5173",
+]
 
-app.include_router(user_logs_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,         
+    allow_credentials=True,
+    allow_methods=["*"],           
+    allow_headers=["*"],           
+)
+
+# app.include_router(user_logs_router)
+# app.include_router(user_router)
+# app.include_router(auth_router)
 
 if __name__ == "__main__":
     uvicorn.run(
